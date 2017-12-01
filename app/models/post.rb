20a71b7d3e4ee8ea833db018e6834f3a1858332d
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-12-01 18:27:20 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   def create
-  post = Post.new
-  post.title = params[:title]
-  post.description = params[:description]
-  post.save
-end
+    post = Post.new
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
+  end
 end
